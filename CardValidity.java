4aa2a1900ad4b002dc7e2ddaee82ade7eb9344ab
SpirituallyRugged <uzomadavidc@gmail.com>
2019-02-12 11:25:46 +0100

    no message

---
+++ CardValidity.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package atmvalidity;
+
+/**
+ *
+ * @author Arazi williams
+ */
+public class CardValidity {
+
+    public static void main(String[] args) {
+    
+       
+        int sum = 0;
+        int array [] = {5,10,4,15,8,20,9,12,25,6,14,30,40,35,22};
+        for (int i=0; i<=14; i++) {
+         sum = sum + array[i];
+            System.out.println(sum);
+            
+           
+        }
+    
+    
+    }
+}
